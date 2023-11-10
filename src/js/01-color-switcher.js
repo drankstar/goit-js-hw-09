67dd3fe15a7278c1a7eabebf2966be48fb6ea685
drankstar <thedrankstar@gmail.com>
2023-11-10 19:34:59 +0200

    hw2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const startBtn = document.querySelector('[data-start]');
+console.log(startBtn);
 
+// const stopBtn = document.querySelector('[data-stop=""]');
+// console.log(stopBtn);
+
+const body = document.querySelector('body');
+console.log(body);
+
+startBtn.addEventListener('click', onClick);
+function onClick() {
+  const color = getRandomHexColor();
+  body.style.background = color;
+}
