c825f4f774a79b2caff07d077869bd4bbc471408
drankstar <137785834+drankstar@users.noreply.github.com>
2023-11-11 16:12:01 +0000

    Deploying to gh-pages from @ drankstar/goit-js-hw-09@0952b9d0345bafd2b4bbc70c3a753f9869473f5c 🚀

---
+++ 01-color-switcher.29d285c1.js
@@ -0,0 +1,2 @@
+!function(){var t,n={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};function e(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));n.body.style.background=t}n.startBtn.addEventListener("click",(function(){t=setInterval(e,1e3),n.startBtn.disabled=!0,n.stopBtn.disabled=!1})),n.stopBtn.disabled=!0,n.stopBtn.addEventListener("click",(function(){clearInterval(t),n.startBtn.disabled=!1,n.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.29d285c1.js.map
